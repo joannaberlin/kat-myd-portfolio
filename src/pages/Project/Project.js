d7a549f8daeb1e4cc6648eedc72dff07ed238d72
joannaberlin <joanna.kot.ok@gmail.com>
2023-06-08 11:24:41 +0200

    Add Project component images rendering

--- src/pages/Project/Project.js
+++ src/pages/Project/Project.js
@@ -1,5 +1,29 @@
+import { useParams } from 'react-router-dom';
+import Container from '../../components/Container/Container';
+import { imagesAll } from '../../items';
+
 const Project = () => {
-	return <h3>You are a beautiful human.</h3>;
+	const { projectId } = useParams();
+	const projectData = imagesAll.filter((items) =>
+		items.projectId === parseInt(projectId) ? items.images : false
+	);
+	console.log(projectData[0].images);
+
+	return (
+		<Container>
+			<div>
+				<h2>Hello</h2>
+				{projectData[0].images.map((images, i) => (
+					<img
+						key={i}
+						src={images.url}
+						alt={images.alt}
+						// className={styles.image}
+					></img>
+				))}
+			</div>
+		</Container>
+	);
 };
 
 export default Project;
